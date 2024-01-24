cb438369bd04372ecca2bd2a5e3917afd9e40cd7
Scott Chamberlain <sachamber@fredhutch.org>
2024-01-24 10:43:12 -0800

    various changes on server and ui focusing app just on proof for now
    - remove the modal to enter a cromwell server url for now, bring back later
    - add new tab in the dashboard for managing cromwell servers and getting metadata on them

--- app/server.R
+++ app/server.R
@@ -6,6 +6,9 @@ library(DT)
 library(tidyverse)
 library(RColorBrewer)
 library(rcromwell)
+library(glue)
+library(shinyBS)
+library(shinyjs)
 # for rendering the About page:
 library(markdown)
 library(shinyWidgets)
@@ -16,12 +19,31 @@ focusID <- 1
 library(proofr)
 library(httr)
 
-proof_loggedin <- function() {
-  !identical(Sys.getenv("PROOF_TOKEN"), "") &&
-    rlang::is_list(cromwell_version())
+proof_wait <- function() {
+  not_up <- TRUE
+  while (not_up) {
+    cromwell_url <- proof_status()$cromwellUrl
+    if (!is.null(cromwell_url)) not_up <- FALSE
+  }
+  cromwell_url
 }
 
-dataModal <- function(failed = FALSE, error = "Invalid username or password") {
+
+proof_loggedin <- function() {
+  !identical(Sys.getenv("PROOF_TOKEN"), "")
+}
+
+cromwell_version_safe <- purrr::safely(cromwell_version, FALSE)
+
+proof_loggedin_serverup <- function() {
+  proof_loggedin() && rlang::is_list(cromwell_version_safe()$result)
+}
+
+proof_serverup <- function() {
+  rlang::is_list(cromwell_version_safe()$result)
+}
+
+loginModal <- function(failed = FALSE, error = "Invalid username or password") {
   modalDialog(
     textInput("username", "Username",
       placeholder = "HutchNet username"
@@ -39,11 +61,33 @@ dataModal <- function(failed = FALSE, error = "Invalid username or password") {
   )
 }
 
+verifyCromwellDeleteModal <- function(failed = FALSE, error = "Woops, an error! Contact DaSL") {
+  modalDialog(
+    title = "Delete your PROOF Cromwell server",
+    "Permanently delete your PROOF Cromwell server. Although you can't undo this action, you can start up another one anytime!",
+    br(),
+    br(),
+    textInput(
+      inputId = "stopCromwell",
+      label = div(HTML("To confirm deletion, type <em>delete me</em> into the field."))
+    ),
+    if (failed) {
+      div(tags$b(error, style = "color: red;"))
+    },
+    footer = tagList(
+      modalButton("Cancel"),
+      shinyjs::disabled(
+        actionButton("deleteCromwell", "Delete", class = "btn-warning")
+      )
+    )
+  )
+}
+
 my.cols <- brewer.pal(6, "RdYlBu")
 
 server <- function(input, output, session) {
   observeEvent(input$proofAuth, {
-    showModal(dataModal())
+    showModal(loginModal())
   })
 
   observeEvent(input$submit, {
@@ -53,73 +97,209 @@ server <- function(input, output, session) {
         error = function(e) e
       )
       if (rlang::is_error(try_auth)) {
-        showModal(dataModal(failed = TRUE, error = try_auth$message))
+        showModal(loginModal(failed = TRUE, error = try_auth$message))
       }
+      # set auth header
       httr::set_config(proof_header())
+      # if cromwell server already up, set that url
+      cromwell_up <- tryCatch(proof_status()$jobStatus, error = function(e) e)
+      print(cromwell_up)
+      if (!rlang::is_error(cromwell_up)) {
+        cromwell_config(proof_wait(), verbose = FALSE)
+      }
+      # remove modal
       removeModal()
     } else {
-      showModal(dataModal(failed = TRUE))
+      showModal(loginModal(failed = TRUE))
     }
   })
 
   # update the icon in the PROOF Login button
   # (FIXME: ideally we change the color of the button too but not sure how to do that yet)
-  observeEvent(proof_loggedin(), {
+  observeEvent(proof_loggedin_serverup(), {
     updateActionButton(session, "proofAuth", icon = icon("unlock"))
   })
 
-  observeEvent(input$getStarted, {
-    inputSweetAlert(
-      session = session, inputId = "cromwellURL", input = "text",
-      title = "What's your server node name and port?",
-      inputPlaceholder = "gizmot32:8000",
-      btn_labels = "Submit"
-    )
-  })
+  # observeEvent(input$getStarted, {
+  #   inputSweetAlert(
+  #     session = session, inputId = "cromwellURL", input = "text",
+  #     title = "What's your server node name and port?",
+  #     inputPlaceholder = "gizmot32:8000",
+  #     btn_labels = "Submit"
+  #   )
+  # })
 
-  theBackends <- eventReactive(input$cromwellURL,
-    {
-      cromwell_config(paste0("https://", input$cromwellURL), verbose = FALSE)
-      try(cromwell_backends()$supportedBackends, silent = TRUE)
-    },
-    ignoreNULL = TRUE
-  )
+  # theBackends <- eventReactive(input$cromwellURL,
+  #   {
+  #     cromwell_config(paste0("https://", input$cromwellURL), verbose = FALSE)
+  #     shiny::validate(
+  #       shiny::need(
+  #         !proof_status()$canJobStart,
+  #         "Your Cromwell server is not running. Go to  the Cromwell servers tab and click Start"
+  #       )
+  #     )
+  #     try(cromwell_backends()$supportedBackends, silent = TRUE)
+  #   },
+  #   ignoreNULL = TRUE
+  # )
 
+  # observeEvent(input$cromwellURL, {
+  #   if (inherits(theBackends(), "try-error")) {
+  #     sendSweetAlert(
+  #       session = session,
+  #       title = "Whoops, that didn't work! If your server has only been running for <1 min, wait and try to reconnect again. ",
+  #       text = print(theBackends()),
+  #       type = "error"
+  #     )
+  #   } else {
+  #     sendSweetAlert(
+  #       session = session,
+  #       title = "Server address valid.",
+  #       text = paste0(
+  #         "Available backends: ",
+  #         paste0(theBackends(), collapse = ", ")
+  #       ),
+  #       type = "success"
+  #     )
+  #   }
+  # })
 
-  observeEvent(input$cromwellURL, {
-    if (inherits(theBackends(), "try-error")) {
-      sendSweetAlert(
-        session = session,
-        title = "Whoops, that didn't work! If your server has only been running for <1 min, wait and try to reconnect again. ",
-        text = print(theBackends()),
-        type = "error"
-      )
-    } else {
-      sendSweetAlert(
-        session = session,
-        title = "Server address valid.",
-        text = paste0(
-          "Available backends: ",
-          paste0(theBackends(), collapse = ", ")
-        ),
-        type = "success"
+  # output$connectionResult <- renderText(
+  #   {
+  #     if (inherits(theBackends(), "try-error")) {
+  #       print(theBackends())
+  #     } else {
+  #       paste0(
+  #         "Server address valid! \n Available backends: ",
+  #         paste0(theBackends(), collapse = ", ")
+  #       )
+  #     }
+  #   },
+  #   quoted = FALSE
+  # )
+
+  ###### Cromwell servers tab ######
+  ## Alert that need to login first
+  observe({
+    if (!proof_loggedin()) {
+      shinyBS::createAlert(session,
+        "alert",
+        title = "Heads up",
+        content = HTML("You aren't logged in. Click the <strong>Proof Login</strong> button to the left"),
+        style = "warning",
+        append = FALSE
       )
     }
   })
 
-  output$connectionResult <- renderText(
-    {
-      if (inherits(theBackends(), "try-error")) {
-        print(theBackends())
+  # Hide or show start and stop buttons
+  observe({
+    if (!proof_loggedin()) shinyjs::hide(id = "cromwellDelete")
+    if (!proof_serverup()) shinyjs::hide(id = "cromwellDelete")
+    if (proof_loggedin_serverup()) {
+      if (proof_status()$canJobStart) {
+        shinyjs::hide(id = "cromwellDelete")
       } else {
-        paste0(
-          "Server address valid! \n Available backends: ",
-          paste0(theBackends(), collapse = ", ")
-        )
+        shinyjs::hide(id = "cromwellStart")
       }
-    },
-    quoted = FALSE
+    }
+  })
+
+  # Start button handling
+  observeEvent(input$cromwellStart, {
+    if (proof_loggedin()) {
+      # fail out early if already running
+      if (!proof_status()$canJobStart) {
+        stop(safeError("Your Cromwell server is already running"))
+      }
+
+      # start cromwell server
+      proof_start()
+
+      # set cromwell server url
+      cromwell_config(proof_wait(), verbose = FALSE)
+      shiny::validate(
+        shiny::need(
+          !proof_status()$canJobStart,
+          "Your Cromwell server is not running. Go to  the Cromwell servers tab and click Start"
+        )
+      )
+
+      # refresh metadata on cromwell servers page
+      print("refresh metadata on cromwell servers page - not actually doing anything")
+
+      # hide/show buttons
+      shinyjs::show(id = "cromwellDelete")
+      shinyjs::hide(id = "cromwellStart")
+    }
+  })
+
+  # Disable or enable the Delete button for deleting proof server
+  observeEvent(input$cromwellDelete, {
+    showModal(verifyCromwellDeleteModal())
+  })
+
+  observe({
+    shinyjs::toggleState("deleteCromwell", input$stopCromwell == "delete me")
+  })
+
+  observeEvent(input$deleteCromwell, {
+    if (input$stopCromwell == "delete me") {
+      try_delete <- tryCatch(proof_cancel(), error = function(e) e)
+      if (rlang::is_error(try_delete)) {
+        showModal(verifyCromwellDeleteModal(failed = TRUE, error = try_delete$message))
+      }
+      removeModal()
+      shinyjs::hide(id = "cromwellDelete")
+      shinyjs::show(id = "cromwellStart")
+    } else {
+      showModal(verifyCromwellDeleteModal(failed = TRUE))
+    }
+  })
+
+  # Gather/show PROOF server status metadata when logged in
+  # OR when user clicks "Update Status" button
+  cromwellProofStatusData <- eventReactive({
+    input$cromwellStatus
+    proof_loggedin_serverup()
+  },
+    proof_status()
   )
+
+  proofStatusTextGenerator <- function(name, list_index) {
+    if (proof_loggedin()) {
+      renderText(
+        paste0(
+          strong(glue("{name}: ")),
+          purrr::flatten(cromwellProofStatusData())[[list_index]]
+        )
+      )
+    }
+  }
+
+  output$proofStatusJobStatus <- proofStatusTextGenerator('Job status', 'jobStatus')
+  output$proofStatusUrlStr <- if (proof_loggedin()) {
+    renderText(
+      paste0(
+        strong("Cromwell URL: "),
+        a(
+          cromwellProofStatusData()$cromwellUrl,
+          target = "_blank",
+          href = cromwellProofStatusData()$cromwellUrl
+        )
+      )
+    )
+  }
+  output$proofStatusWorkflowLogDir <- proofStatusTextGenerator('Workflow log directory', 'WORKFLOWLOGDIR')
+  output$proofStatusScratchDir <- proofStatusTextGenerator('Scratch directory', 'SCRATCHDIR')
+  output$proofStatusSlurmJobId <- proofStatusTextGenerator('Slurm job ID', 'SLURM_JOB_ID')
+  output$proofStatusCromwellDir <- proofStatusTextGenerator('Cromwell directory', 'CROMWELL_DIR')
+  output$proofStatusServerLogDir <- proofStatusTextGenerator('Server log directory', 'SERVERLOGDIR')
+  output$proofStatusSingularityCacheDir <- proofStatusTextGenerator('Singlarity cache directory', 'SINGULARITY_CACHEDIR')
+  output$proofStatusServerTime <- proofStatusTextGenerator('Server time zone', 'SERVERTIME')
+  output$proofStatusUseAWS <- proofStatusTextGenerator('Use AWS?', 'USE_AWS')
+  output$proofStatusSlurmJobAccount <- proofStatusTextGenerator('Slurm job account', 'SLURM_JOB_ACCOUNT')
+
   ###### Cromwell Submit tab ######
   ## Validate a possible workflow
   validateWorkflow <- eventReactive(input$validateWorkflow,
