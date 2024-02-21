03a65854ceb23b92d64978997a930873cff33ec0
Scott Chamberlain <sachamber@fredhutch.org>
2024-02-21 00:46:48 -0800

    allow for trailing slash in cromwell url validator

--- app/validators.R
+++ app/validators.R
@@ -9,7 +9,7 @@ validatorOwnCromwell <- function(x) {
   iv$add_rule("ownCromwellURL", sv_required())
   # URL must match this regex
   iv$add_rule("ownCromwellURL",
-    sv_regex("^https?://[a-zA-Z0-9]*:[0-9]*$", "Doesn't match pattern"))
+    sv_regex("^https?://[a-zA-Z0-9]*:[0-9]*/?$", "Doesn't match pattern"))
   # URL must be a valid URL via rcromwell::cromwell_version
   iv$add_rule("ownCromwellURL",
     ~ if (!check_url(.)) "Not a valid Cromwell URL")
