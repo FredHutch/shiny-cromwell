9b9850ed16d53fcd0664d697e908cf6ac936b7a9
Scott Chamberlain <sachamber@fredhutch.org>
2024-02-27 22:24:24 -0800

    troubleshoot text: use "Workflow id" instead of "PROOF workflow id"

--- app/tab-troubleshoot.R
+++ app/tab-troubleshoot.R
@@ -33,7 +33,7 @@ tab_troublehsoot <- tabItem(
       p("When a workflow fails but no jobs were started, or there appears to be no clear reason for a workflow to have failed, this tool can provide you the entire set of workflow metadata Cromwell has for your workflow in it's raw and unprocessed (json) form. For complex workflows, this can be rather large (and ugly!)."),
       textInput(
         inputId = "troubleWorkflowID",
-        label = "PROOF workflow id to get metadata for:",
+        label = "Workflow id to get metadata for:",
         value = "",
         placeholder = "577b9aa4-b26b-4fd6-9f17-7fb33780bbd0",
         width = "30%"
