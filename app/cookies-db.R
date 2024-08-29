e7a6b88d502fb2b5550761157662c91e202cac21
Scott Chamberlain <sachamber@fredhutch.org>
2024-08-29 13:46:53 -0700

    cookies funs:
    - use bang bang in dplyr NSE string so that we get the value of user
    - drop from db: use dbExecute instead of dbGetQuery as per warning
    - user add to db: new drop_existing param to drop existing rows of the user before adding

--- app/cookies-db.R
+++ app/cookies-db.R
@@ -31,13 +31,22 @@ user_from_db <- function(user, conn = make_db_con(), expiry = COOKIE_EXPIRY_DAYS
   dbReadTable(conn, "users") %>%
     as_tibble() %>%
     filter(
-      user == user,
+      # !! needed to get the value of the variable
+      user == !!user,
       login_time > now() - days(expiry)
     ) %>%
     arrange(desc(login_time))
 }
-user_to_db <- function(user, token, url, conn = make_db_con()) {
+user_to_db <- function(user, token, url, drop_existing = FALSE, conn = make_db_con()) {
   on.exit(dbDisconnect(conn))
+  # drop before inserting so we only have 1 row/user
+  if (drop_existing) {
+    # don't drop if user is empty
+    if (nzchar(user)) {
+      # don't disconnect from within drop fun
+      user_drop_from_db(user, disconnect = FALSE, conn = conn)
+    }
+  }
   tibble(
     user = user,
     proof_token = token,
@@ -46,11 +55,13 @@ user_to_db <- function(user, token, url, conn = make_db_con()) {
   ) %>%
     dbWriteTable(conn, "users", ., append = TRUE)
 }
-user_drop_from_db <- function(user, conn = make_db_con()) {
-  on.exit(dbDisconnect(conn))
+user_drop_from_db <- function(user, disconnect = TRUE, conn = make_db_con()) {
+  if (disconnect) {
+    on.exit(dbDisconnect(conn))
+  }
   sql_delete <- glue::glue_sql("
     DELETE from users
     WHERE user = {user}
   ", .con = conn)
-  dbGetQuery(conn, sql_delete)
+  dbExecute(conn, sql_delete)
 }
